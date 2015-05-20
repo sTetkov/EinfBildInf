b31c2c97b6f958a6cfaca1c46ca5f6e3b6a29f3f
Sascha Tetkov <sascha.tetkov@uni-jena.de>
2015-05-20 22:37:41 +0200

    Excercise 2 time measurement
    Added time measurement and polygon for excercise 3

--- aufgabe_02_01.cpp
+++ aufgabe_02_01.cpp
@@ -132,6 +132,11 @@ void BresenhamCircle(int x0, int y0, int radius, cv::Mat canvas)
   }
 }
 
+bool isInPolygon(cv::Point a,cv::Point b,cv::Point c,cv::Point x)
+{
+  return false;
+}
+
 int main(int argc, char** argv)
 {
   std::cout << "**************************************************\n"
@@ -143,6 +148,7 @@ int main(int argc, char** argv)
   int w=1200;
   int h=600;
 
+  double t;
   cv::Mat canvas=cv::Mat::zeros( h, w, CV_8UC3);
   srand(42);
   cv::line(canvas,cv::Point(w/3,0),cv::Point(w/3,h),cv::Scalar(0,254,0),1,8);
@@ -153,8 +159,15 @@ int main(int argc, char** argv)
     int y1=rand() % h;
     int x2=x1+rand() % ((w/3)-x1);
     int y2=y1+rand() % (h-y1);
+
+    t=(double)cv::getTickCount();
     DrawLineBresenham(x1,y1,x2,y2,canvas);
+    t=((double)cv::getTickCount()-t)/cv::getTickFrequency();
+    std::cout<<"Bresenham Line: "<<t<<" seconds\n";
+    t=(double)cv::getTickCount();
     cv::line(canvas,cv::Point(x1+(w/3),y1),cv::Point(x2+(w/3),y2),cv::Scalar(254,254,254),1,8);
+    t=((double)cv::getTickCount()-t)/cv::getTickFrequency();
+    std::cout<<"OpenCV Line: "<<t<<" seconds\n";
   }
   cv::namedWindow("My Image Window", CV_WINDOW_AUTOSIZE || CV_WINDOW_KEEPRATIO);
   cv::imshow("My Image Window", canvas);// show image
@@ -166,12 +179,54 @@ int main(int argc, char** argv)
   cv::line(canvas,cv::Point(w/3,0),cv::Point(w/3,h),cv::Scalar(0,254,0),1,8);
   cv::line(canvas,cv::Point(2*(w/3),0),cv::Point(2*(w/3),h),cv::Scalar(0,254,0),1,8);
 
+  t=(double)cv::getTickCount();
   BresenhamCircle(200,300,190,canvas);
+  t=((double)cv::getTickCount()-t)/cv::getTickFrequency();
+  std::cout<<"Bresenham Circle: "<<t<<" seconds\n";
+
+  t=(double)cv::getTickCount();
   cv::circle(canvas,cv::Point(600,300),190,cv::Scalar(254,254,254));
+  t=((double)cv::getTickCount()-t)/cv::getTickFrequency();
+  std::cout<<"OpenCv Circle: "<<t<<" seconds\n";
 
   cv::imshow("My Image Window", canvas);// show image
   std::cout << "Press any key to continue ...\n";
   cv::waitKey();                     // wait for key press
 
+  canvas=cv::Mat::zeros( h, w, CV_8UC3);
+
+  cv::Point a=cv::Point(10,10);
+  cv::Point b=cv::Point(900,450);
+  cv::Point c=cv::Point(30,500);
+  cv::line(canvas,a,b,cv::Scalar(254,254,254),1,8);
+  cv::line(canvas,b,c,cv::Scalar(254,254,254),1,8);
+  cv::line(canvas,c,a,cv::Scalar(254,254,254),1,8);
+
+  cv::Vec3b dotColor;
+  dotColor[0]=254;
+  dotColor[1]=254;
+  dotColor[2]=254;
+
+  for (int i=0; i<200;i++)
+  {
+    int x=rand() % w;
+    int y=rand() % h;
+    if(isInPolygon(a,b,c,cv::Point(x,y)))
+    {
+      dotColor[1]=0;
+      dotColor[2]=0;
+      canvas.at<cv::Vec3b>(cv::Point(x,y))=dotColor;
+    }
+    else
+    {
+      dotColor[1]=254;
+      dotColor[2]=254;
+      canvas.at<cv::Vec3b>(cv::Point(x,y))=dotColor;
+    }
+  }
+
+  cv::imshow("My Image Window", canvas);// show image
+  std::cout << "Press any key to continue ...\n";
+  cv::waitKey();                     // wait for key press
 
 }
