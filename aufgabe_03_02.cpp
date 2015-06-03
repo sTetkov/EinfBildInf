43e4e15114e948e7c41caa7557e86ef2d6a61ee8
Sascha Tetkov <sascha.tetkov@uni-jena.de>
2015-06-03 17:59:26 +0200

    Excercise Series 3
    Finished Excercise 03_02

--- aufgabe_03_02.cpp
+++ aufgabe_03_02.cpp
@@ -18,18 +18,18 @@ int clickCounter;
   * @param m multiplicative matrix
   * @param t additive vector
   */
-PointType transform(PointType point,const double m[2 * 2]) {
+PointType transform(PointType point,const double* m) {
     double x = point.first;            // get point coordinates
     double y = point.second;
-     
-    double x1=x*m[0]+y*m[1]+t[0];
-    double y1=x*m[2]+y*m[3]+t[1];
+
+    double x1=x*m[0]+y*m[1];
+    double y1=x*m[2]+y*m[3];
 
     PointType res(x1,y1);
     return res;
 }
 
-double[2*2] getTransformationMatrix(PointType p1, PointType p2) {
+double* getTransformationMatrix(PointType p1, PointType p2) {
 	double x=0;
 	double y=0;
 	if(p2.first>=p1.first && p2.second>=p1.second) //First quarter
@@ -52,31 +52,37 @@ double[2*2] getTransformationMatrix(PointType p1, PointType p2) {
 		x=p1.first-p2.first;
 		y=p2.second-p1.second;
 	}
-
 	double angle=atan2(y,x);
-	double m[2 * 2] = {cos(angle), sin(angle),
-                          -sin(angle), cos(angle)};
+	double *m = new double[2*2];
+  m[0]=cos(angle);
+  m[1]=sin(angle);
+  m[2]=-sin(angle);
+  m[3]=cos(angle);
+  std::cout<<"Transformation Matrix: "<<m[0]<<"|"<<m[1]<<"\n"
+           <<"                       "<<m[2]<<"|"<<m[3]<<"\n";
 	return m;
 }
 
 
-int colorVal(cv::Mat& img,PointType p, double m[2*2])
+int colorVal(cv::Mat& img,PointType p, double* m)
 {
+  p.first-=p1.first;
+  p.second-=p1.second;
 	PointType q=transform(p,m);
 	cv::Size s = img.size();
-	int x=(int)std::round(q.first);
-	int y=(int)std::round(q.second);
+	int x=(int)(q.first+0.5)+p1.first;
+	int y=(int)(q.second+0.5)+p1.second;
 	if(x<0 || x>=s.width || y<0 || y>=s.height)
 		return 0;
-	return img.at<uchar>(Point(x,y));
-
+  int res=img.at<uchar>(cv::Point(x,y));
+  return res;
 }
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
-     if  ( event == EVENT_LBUTTONDOWN )
+     if  ( event == cv::EVENT_LBUTTONDOWN )
      {
-          cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+          std::cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")\n";
 	  if(clickCounter==0)
 	  {
 		  p1.first=x;
@@ -97,23 +103,28 @@ int main(int argc, char **argv) {
             << "*** Excercise Series 3 - Sascha Tetkov         ***\n"
             << "*** Exercise 03-2                              ***\n"
             << "**************************************************\n";
-  
+
   clickCounter=0;
-  cv::Mat img=imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-  double m[2*2]=getTransformationMatrix(p1,p2);
+  cv::Mat img=cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+
   cv::Size s = img.size();
 
   cv::namedWindow("Image", CV_WINDOW_AUTOSIZE || CV_WINDOW_KEEPRATIO);
-  cv::imshow("Image", imgPost);// show image
   cv::setMouseCallback("Image", CallBackFunc, NULL);
-  while(clickCounter<2)
-  {}
+  cv::imshow("Image", img);// show image
 
-  cv::Mat imgPost=cv::Mat::zeros(s.width,s.height,uchar);
+  std::cout << "Press any key to continue ...\n";
+  cv::waitKey();                     // wait for key press
+
+
+  double* m=getTransformationMatrix(p1,p2);
+  cv::Mat imgPost=cv::Mat::zeros(s.height,s.width,cv::DataType<uchar>::type);
   for (int x=0;x<s.width;x++)
 	  for(int y=0;y<s.height;y++)
-		  imgPost.at<uchar>(Point(x,y))=colorVal(img,PointType(x,y),m);
-  
+    {
+		  imgPost.at<uchar>(cv::Point(x,y))=colorVal(img,PointType(x,y),m);
+    }
+
   cv::namedWindow("Rotation", CV_WINDOW_AUTOSIZE || CV_WINDOW_KEEPRATIO);
   cv::imshow("Rotation", imgPost);// show image
   std::cout << "Press any key to continue ...\n";
